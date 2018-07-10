ce6adf74725e08bd7bde8887f1ca29deda1b6faa
Gerry Sousa <gerry.sousa@prodaptech.com.br>
2018-07-10 10:01:31 -0300

    Seção 3, aula 12. Campo de Treinamento Appium

---
+++ CursoAppium/src/main/java/br/ce/gsousa/appium/FormularioTeste.java
@@ -0,0 +1,30 @@
+package br.ce.gsousa.appium;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+
+public class FormularioTeste {
+	
+	@Test
+	public  void deveInstalarAPK() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+	    desiredCapabilities.setCapability("platformName", "Android");
+	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
+	    desiredCapabilities.setCapability("automationName", "uiautomator2");
+	    desiredCapabilities.setCapability(MobileCapabilityType.APP, "D:\\Treinamento\\testes-appium\\CursoAppium\\src\\main\\resources\\CTAppium-1-1.apk");
+	    //desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+	    	    
+	    AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+	    
+	  
+	    driver.quit();
+	}
+}
