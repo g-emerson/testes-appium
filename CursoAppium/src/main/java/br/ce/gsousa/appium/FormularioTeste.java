3dd3446fda1d970475ae294d8a99fee4c0f92580
Gerry Sousa <gerry.sousa@prodaptech.com.br>
2018-07-10 10:36:25 -0300

    Seção 3, aula 14. EditText

--- CursoAppium/src/main/java/br/ce/gsousa/appium/FormularioTeste.java
+++ CursoAppium/src/main/java/br/ce/gsousa/appium/FormularioTeste.java
@@ -1,11 +1,19 @@
 package br.ce.gsousa.appium;
 
+import static org.junit.Assert.assertArrayEquals;
+
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -23,7 +31,19 @@ public class FormularioTeste {
 	    //desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
 	    	    
 	    AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	    
+	    //selecionar formulario
+	   List<MobileElement> elementosEncontrados = driver.findElements(By.className("android.widget.TextView"));
+	   elementosEncontrados.get(1).click();
+	   
+	    //escrever nome
+	   MobileElement campoNome = driver.findElement(MobileBy.AccessibilityId("nome"));
+	    campoNome.sendKeys("Gerry");
+	    
+	    //verificar nome escrito
+	    String texto = campoNome.getText();
+	    Assert.assertEquals("Gerry", texto);
 	  
 	    driver.quit();
 	}
