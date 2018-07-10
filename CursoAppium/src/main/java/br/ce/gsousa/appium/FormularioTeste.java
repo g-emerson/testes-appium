42279c0011168cb8dee2d2870ebcc794280d37ba
Gerry Sousa <gerry.sousa@prodaptech.com.br>
2018-07-10 11:47:36 -0300

    Seção 3, aula 15. Combo

--- CursoAppium/src/main/java/br/ce/gsousa/appium/FormularioTeste.java
+++ CursoAppium/src/main/java/br/ce/gsousa/appium/FormularioTeste.java
@@ -22,7 +22,7 @@ import io.appium.java_client.remote.MobileCapabilityType;
 public class FormularioTeste {
 	
 	@Test
-	public  void deveInstalarAPK() throws MalformedURLException {
+	public  void devePreencherCampoTexto() throws MalformedURLException {
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 	    desiredCapabilities.setCapability("platformName", "Android");
 	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
@@ -44,6 +44,36 @@ public class FormularioTeste {
 	    //verificar nome escrito
 	    String texto = campoNome.getText();
 	    Assert.assertEquals("Gerry", texto);
+	  
+	    driver.quit();
+	}
+	
+	@Test
+	public  void deveInteragirCombo() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+	    desiredCapabilities.setCapability("platformName", "Android");
+	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
+	    desiredCapabilities.setCapability("automationName", "uiautomator2");
+	    desiredCapabilities.setCapability(MobileCapabilityType.APP, "D:\\Treinamento\\testes-appium\\CursoAppium\\src\\main\\resources\\CTAppium-1-1.apk");
+	    //desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+	    	    
+	    AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    
+	    //selecionar formulario
+	    driver.findElement(By.xpath("//android.widget.TextView[@text='Formulário']")).click();
+	   
+	    //clicar no combo
+	    driver.findElement(MobileBy.AccessibilityId("console")).click();
+	   
+	   	//selecionar opcao desejada Nitendo Switch
+	    driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='Nintendo Switch']")).click();
+	    
+	    //verificar nome escrito 
+	    String texto = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
+	    
+	    //String texto = campoNome.getText();
+	    Assert.assertEquals("Nintendo Switch", texto);
 	  
 	    driver.quit();
 	}
