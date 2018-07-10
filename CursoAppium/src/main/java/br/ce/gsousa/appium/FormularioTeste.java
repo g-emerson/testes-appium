464cec3d314308b31cffa0916a99a1a837eccc56
Gerry Sousa <gerry.sousa@prodaptech.com.br>
2018-07-10 12:11:54 -0300

    Seção 3, aula 16. Switch / Checkbox

--- CursoAppium/src/main/java/br/ce/gsousa/appium/FormularioTeste.java
+++ CursoAppium/src/main/java/br/ce/gsousa/appium/FormularioTeste.java
@@ -49,7 +49,7 @@ public class FormularioTeste {
 	}
 	
 	@Test
-	public  void deveInteragirCombo() throws MalformedURLException {
+	public  void deveInteragirComCombo() throws MalformedURLException {
 		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 	    desiredCapabilities.setCapability("platformName", "Android");
 	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
@@ -74,6 +74,38 @@ public class FormularioTeste {
 	    
 	    //String texto = campoNome.getText();
 	    Assert.assertEquals("Nintendo Switch", texto);
+	  
+	    driver.quit();
+	}
+	
+	@Test
+	public  void deveInteragirComSwitchCheckBox() throws MalformedURLException {
+		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+	    desiredCapabilities.setCapability("platformName", "Android");
+	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
+	    desiredCapabilities.setCapability("automationName", "uiautomator2");
+	    desiredCapabilities.setCapability(MobileCapabilityType.APP, "D:\\Treinamento\\testes-appium\\CursoAppium\\src\\main\\resources\\CTAppium-1-1.apk");
+	    //desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+	    	    
+	    AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	    
+	    //selecionar formulario
+	    driver.findElement(By.xpath("//*[@text='Formulário']")).click();
+	   
+	   //verificar status dos elementos
+	   MobileElement check = driver.findElement(By.className("android.widget.CheckBox"));
+	   MobileElement switc = driver.findElement(MobileBy.AccessibilityId("switch"));
+	   Assert.assertTrue(check.getAttribute("checked").equals("false"));
+	   Assert.assertTrue(switc.getAttribute("checked").equals("true"));
+	   
+	    //clicar nos elementos
+	   check.click();
+	   switc.click();
+	   
+	    //verificar status alterados
+	   Assert.assertFalse(check.getAttribute("checked").equals("false"));
+	   Assert.assertFalse(switc.getAttribute("checked").equals("true"));
 	  
 	    driver.quit();
 	}
