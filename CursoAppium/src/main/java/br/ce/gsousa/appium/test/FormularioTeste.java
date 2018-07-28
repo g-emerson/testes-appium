45686bb73812e4cfbee6791e880df89e295e0d8d
Gerry Sousa <gerry.con@hotmail.com>
2018-07-28 15:27:07 -0300

    Seção 7, DatePicker

--- CursoAppium/src/main/java/br/ce/gsousa/appium/test/FormularioTeste.java
+++ CursoAppium/src/main/java/br/ce/gsousa/appium/test/FormularioTeste.java
@@ -18,69 +18,78 @@ import br.ce.gsousa.appium.core.DriverFactory;
 import br.ce.gsousa.appium.page.FormularioPage;
 import br.ce.gsousa.appium.page.MenuPage;
 
-public class FormularioTeste extends BaseTest{
+public class FormularioTeste extends BaseTest {
 	private MenuPage menu = new MenuPage();
 	private FormularioPage page = new FormularioPage();
-	
+
 	@Before
 	public void inicializarAppium() throws MalformedURLException {
-		//driver = DriverFactory.getDriver();   
-		menu.acessarFormulario();	  	    
+		// driver = DriverFactory.getDriver();
+		menu.acessarFormulario();
 	}
-	
-		
-	
+
 	@Test
-	public  void devePreencherCampoTexto() throws MalformedURLException {
-	    page.escreverNome("Gerry");
-	   
-	    assertEquals("Gerry", page.obterNome());
+	public void devePreencherCampoTexto() throws MalformedURLException {
+		page.escreverNome("Gerry");
+
+		assertEquals("Gerry", page.obterNome());
 	}
-	
+
 	@Test
-	public  void deveInteragirComCombo() throws MalformedURLException {
+	public void deveInteragirComCombo() throws MalformedURLException {
 		page.selecionarCombo("Nintendo Switch");
 
 		Assert.assertEquals("Nintendo Switch", page.obterValorCombo());
 	}
-	
+
 	@Test
-	public  void deveInteragirComSwitchCheckBox() throws MalformedURLException {
+	public void deveInteragirComSwitchCheckBox() throws MalformedURLException {
 		Assert.assertFalse(page.isCheckMarcado());
 		Assert.assertTrue(page.isSwitchMarcado());
-		
+
 		page.clicarCheck();
 		page.clicarSwitch();
-		
+
 		Assert.assertTrue(page.isCheckMarcado());
 		Assert.assertFalse(page.isSwitchMarcado());
 	}
-	
+
 	@Test
-	public  void deveRealizarCadastro() throws MalformedURLException {
+	public void deveRealizarCadastro() throws MalformedURLException {
 		page.escreverNome("Gerry");
 		page.clicarCheck();
 		page.clicarSwitch();
 		page.selecionarCombo("PS4");
 		page.salvar();
-	
-	    assertEquals("Nome: Gerry", page.obterNomeCadastrato());
-	    assertEquals("Console: ps4", page.obterConsoleCadastrado());
-	    assertTrue(page.obterCheckCadastrado().endsWith("Off"));
-	    assertTrue(page.obterSwithCadastrado().endsWith("Marcado"));
-		}
-	
+
+		assertEquals("Nome: Gerry", page.obterNomeCadastrato());
+		assertEquals("Console: ps4", page.obterConsoleCadastrado());
+		assertTrue(page.obterCheckCadastrado().endsWith("Off"));
+		assertTrue(page.obterSwithCadastrado().endsWith("Marcado"));
+	}
+
 	@Test
-	public  void deveRealizarCadastroDemorado() throws MalformedURLException {
+	public void deveRealizarCadastroDemorado() throws MalformedURLException {
 		page.escreverNome("Gerry");
 		DriverFactory.getDriver().manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-		
+
 		page.salvarDemorado();
-		//esperar(3000);
-		
+		// esperar(3000);
+
 		WebDriverWait wait = new WebDriverWait(DriverFactory.getDriver(), 10);
 		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@text='Nome: Gerry']")));
-		
-	    assertEquals("Nome: Gerry", page.obterNomeCadastrato());
-		}
+
+		assertEquals("Nome: Gerry", page.obterNomeCadastrato());
+	}
+
+	@Test
+	public void deveMudarData() {
+		page.clicarTexto("01/01/2000");
+		page.clicarTexto("20");
+		page.clicarTexto("OK");
+
+		Assert.assertTrue(page.existeElementoPorTexto("20/2/2000"));
+
+	}
+
 }
