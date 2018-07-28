080742185b38c0d82167621ecb13fad1c67ca84c
Gerry Sousa <gerry.con@hotmail.com>
2018-07-28 14:03:27 -0300

    Seção 6, aula 28. Tabs... colocando o caminho do app relativo

--- CursoAppium/src/main/java/br/ce/gsousa/appium/core/DriverFactory.java
+++ CursoAppium/src/main/java/br/ce/gsousa/appium/core/DriverFactory.java
@@ -1,5 +1,7 @@
 package br.ce.gsousa.appium.core;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -12,36 +14,43 @@ import io.appium.java_client.remote.MobileCapabilityType;
 
 public class DriverFactory {
 
-	private static AndroidDriver<MobileElement> driver ;
-	
-	public static AndroidDriver<MobileElement>getDriver() {
-		if(driver==null)
-		{
+	private static AndroidDriver<MobileElement> driver;
+
+	public static AndroidDriver<MobileElement> getDriver() {
+		if (driver == null) {
 			createDrive();
 		}
 		return driver;
 	}
-	private static void createDrive(){
-		DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-	    desiredCapabilities.setCapability("platformName", "Android");
-	    desiredCapabilities.setCapability("deviceName", "emulator-5554");
-	    desiredCapabilities.setCapability("automationName", "uiautomator2");
-	    desiredCapabilities.setCapability(MobileCapabilityType.APP, "D:\\Treinamento\\testes-appium\\CursoAppium\\src\\main\\resources\\CTAppium-1-1.apk");
-	     	    
-	    try {
-			driver = new AndroidDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
-		} catch (MalformedURLException e) {
+
+	private static void createDrive() {
+		try {
+			String apkCaminho = new File("../CursoAppium/src/main/resources/CTAppium-1-1.apk").getCanonicalPath();
+
+			DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+			desiredCapabilities.setCapability("platformName", "Android");
+			desiredCapabilities.setCapability("deviceName", "emulator-5554");
+			desiredCapabilities.setCapability("automationName", "uiautomator2");
+			desiredCapabilities.setCapability(MobileCapabilityType.APP, apkCaminho);
+
+			try {
+				driver = new AndroidDriver<MobileElement>(new URL("http://localhost:4723/wd/hub"), desiredCapabilities);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		} catch (IOException e1) {
 			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e1.printStackTrace();
 		}
-	    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-	}	
-	
+	}
+
 	public static void killDriver() {
-		if(driver!=null)
-		{
-		driver.quit();
-		driver=null;
+		if (driver != null) {
+			driver.quit();
+			driver = null;
 		}
-	}	
+	}
 }
